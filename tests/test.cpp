592de9a8637f4ff45064a1eb1a1c7d1f379fc780
kewuaa <202321011111@std.uestc.edu.cn>
2025-08-24 03:23:23 +0800

    add tests

---
+++ tests/test.cpp
@@ -0,0 +1,34 @@
+#include <map>
+#include <cassert>
+#include <iostream>
+
+#include "skip_list.hpp"
+
+
+int main() {
+    std::map<int, int> m;
+    SkipList<std::string, int, 3> sl;
+    sl["kewuaa"] = 1;
+    sl.insert("1", 111);
+    sl.insert("9", 999);
+    sl.insert("5", 555);
+    sl.insert("hello", 0);
+    std::cout << sl << std::endl;
+    if (auto v = sl.find("5"); v) {
+        std::cout << "found 5 -> " << v->second << std::endl;
+    } else {
+        std::cout << "5 not found" << std::endl;
+    }
+    sl.remove("5");
+    std::cout << sl << std::endl;
+    if (auto v = sl.find("5"); v) {
+        std::cout << "found " << v->second << std::endl;
+    } else {
+        std::cout << "5 not found" << std::endl;
+    }
+
+    for (auto& [key, value] : sl) {
+        std::cout << key << ": " << value << std::endl;
+    }
+    std::cout << sl["hello"] << std::endl;
+}
